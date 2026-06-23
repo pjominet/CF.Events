17cf16f846906588f76b92fa247fa17532a12ec6
Patrick Jominet <patrick.jominet@technologypartner.io>
2026-06-23 19:04:00 +0200

    deleted old mock file

--- CF.Events.Web/Data/mocking/mock-rsvps.sql
+++
@@ -1,3 +0,0 @@
-﻿-- This file needs updating to match the new ApplicationUser and Event based RSVP system.
--- For now, it's disabled as it doesn't match the new schema.
--- INSERT INTO Rsvps (EventId, UserId, Attending, BringsPlusOne, JoinForDinner, Comments, SubmittedAt) VALUES ...
