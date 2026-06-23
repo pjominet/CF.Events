6fac8a7a9bde97c248ab217b0f4b864cd4e0476f
Patrick Jominet <patrick.jominet@technologypartner.io>
2026-06-23 11:57:39 +0200

    added mailjet email sender

--- CF.Events.Web/Infrastructure/Settings/AppSettings.cs
+++ CF.Events.Web/Infrastructure/Settings/AppSettings.cs
@@ -3,5 +3,13 @@
 public class AppSettings
 {
     public required string Name { get; init; }
-    public required string TransactionalEmailApiKey { get; init; }
+    public required MailjetSettings Mailjet { get; init; }
+}
+
+public class MailjetSettings
+{
+    public required string ApiKey { get; init; }
+    public required string ApiSecret { get; init; }
+    public required string SenderEmail { get; init; }
+    public required string SenderName { get; init; }
 }
