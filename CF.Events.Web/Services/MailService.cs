53aa9f674d224f93ce7b76c25dd1055bee746022
Patrick Jominet <patrick.jominet@technologypartner.io>
2026-07-08 14:29:53 +0200

    updated MailjetEmailSender and MailService

--- CF.Events.Web/Services/MailService.cs
+++ CF.Events.Web/Services/MailService.cs
@@ -1,43 +1,40 @@
-﻿using Mailjet.Client;
+﻿using CF.Events.Web.Infrastructure.Settings;
+using Mailjet.Client;
 using Mailjet.Client.Resources;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json.Linq;
 
 namespace CF.Events.Web.Services;
 
-public class MailService(IMailjetClient mailjetClient) : MailjetService(mailjetClient), IMailService
+public class MailService(IMailjetClient mailjetClient, IOptions<AppSettings> settings) : MailjetService(mailjetClient), IMailService
 {
+    private readonly MailjetSettings _mailjetSettings = settings.Value.Mailjet;
+
     public async Task SendInvitationAsync(string eventName, string displayName, string email, string callBackUrl, string? customDesign = null, CancellationToken ctx = default)
     {
         var request = new MailjetRequest
             {
                 Resource = Send.Resource
             }
-            .Property(Send.Messages, new JArray
+            .Property(Send.FromEmail, _mailjetSettings.SenderEmail)
+            .Property(Send.FromName, _mailjetSettings.SenderName)
+            .Property(Send.Recipients, new JArray
             {
                 new JObject
                 {
-                    {
-                        Send.To, new JArray
-                        {
-                            new JObject
-                            {
-                                { "Email", email },
-                                { "Name", displayName }
-                            }
-                        }
-                    },
-                    { "TemplateID", 0 },
-                    {
-                        "Variables", new JObject
-                        {
-                            { "sender_sig", "Patrick & Éadaoin" },
-                            { "invitation_callback", callBackUrl },
-                            { "display_name", displayName },
-                            { "event_name", eventName },
-                            { "custom_design", customDesign }
-                        }
-                    }
+                    { "Email", email },
+                    { "Name", displayName }
                 }
+            })
+            .Property("Mj-TemplateID", 0)
+            .Property("Mj-TemplateLanguage", true)
+            .Property("Variables", new JObject
+            {
+                { "sender_sig", "Patrick & Éadaoin" },
+                { "invitation_callback", callBackUrl },
+                { "display_name", displayName },
+                { "event_name", eventName },
+                { "custom_design", customDesign }
             });
 
         await SendMailjetEmailAsync(request, ctx);
@@ -49,31 +46,24 @@ public class MailService(IMailjetClient mailjetClient) : MailjetService(mailjetC
             {
                 Resource = Send.Resource
             }
-            .Property(Send.Messages, new JArray
+            .Property(Send.FromEmail, _mailjetSettings.SenderEmail)
+            .Property(Send.FromName, _mailjetSettings.SenderName)
+            .Property(Send.Recipients, new JArray
             {
                 new JObject
                 {
-                    {
-                        Send.To, new JArray
-                        {
-                            new JObject
-                            {
-                                { "Email", email },
-                                { "Name", displayName }
-                            }
-                        }
-                    },
-                    { "TemplateID", templateId },
-                    {
-                        "Variables", new JObject
-                        {
-                            { "sender_sig", "Patrick & Éadaoin" },
-                            { "display_name", displayName },
-                            { "event_name", eventName },
-                            { "return_url", returnUrl }
-                        }
-                    }
+                    { "Email", email },
+                    { "Name", displayName }
                 }
+            })
+            .Property("Mj-TemplateID", templateId)
+            .Property("Mj-TemplateLanguage", true)
+            .Property("Variables", new JObject
+            {
+                { "sender_sig", "Patrick & Éadaoin" },
+                { "display_name", displayName },
+                { "event_name", eventName },
+                { "return_url", returnUrl }
             });
 
         await SendMailjetEmailAsync(request, ctx);
--- CF.Events.Web/Services/MailjetEmailSender.cs
+++ CF.Events.Web/Services/MailjetEmailSender.cs
@@ -1,44 +1,41 @@
-﻿using CF.Events.Web.Models;
+﻿using CF.Events.Web.Infrastructure.Settings;
+using CF.Events.Web.Models;
 using Mailjet.Client;
 using Mailjet.Client.Resources;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json.Linq;
 
 namespace CF.Events.Web.Services;
 
-public class MailjetEmailSender(IMailjetClient mailjetClient) : MailjetService(mailjetClient), IEmailSender<AppUser>
+public class MailjetEmailSender(IMailjetClient mailjetClient, IOptions<AppSettings> settings) : MailjetService(mailjetClient), IEmailSender<AppUser>
 {
+    private readonly MailjetSettings _mailjetSettings = settings.Value.Mailjet;
+
     public async Task SendConfirmationLinkAsync(AppUser user, string email, string confirmationLink)
     {
         var request = new MailjetRequest
             {
                 Resource = Send.Resource
             }
-            .Property(Send.Messages, new JArray
+            .Property(Send.FromEmail, _mailjetSettings.SenderEmail)
+            .Property(Send.FromName, _mailjetSettings.SenderName)
+            .Property(Send.Recipients, new JArray
             {
                 new JObject
                 {
-                    {
-                        Send.To, new JArray
-                        {
-                            new JObject
-                            {
-                                { "Email", email },
-                                { "Name", user.DisplayName }
-                            }
-                        }
-                    },
-                    { "TemplateID", 8135949 },
-                    {
-                        "Variables", new JObject
-                        {
-                            { "sender_sig", "Patrick & Éadaoin" },
-                            { "confirmation_link", confirmationLink },
-                            { "display_name", user.DisplayName }
-                        }
-                    }
+                    { "Email", email },
+                    { "Name", user.DisplayName }
                 }
+            })
+            .Property("Mj-TemplateID", 8135949)
+            .Property("Mj-TemplateLanguage", true)
+            .Property("Variables", new JObject
+            {
+                { "sender_sig", "Patrick & Éadaoin" },
+                { "confirmation_link", confirmationLink },
+                { "display_name", user.DisplayName }
             });
 
         await SendMailjetEmailAsync(request);
@@ -50,30 +47,23 @@ public class MailjetEmailSender(IMailjetClient mailjetClient) : MailjetService(m
             {
                 Resource = Send.Resource
             }
-            .Property(Send.Messages, new JArray
+            .Property(Send.FromEmail, _mailjetSettings.SenderEmail)
+            .Property(Send.FromName, _mailjetSettings.SenderName)
+            .Property(Send.Recipients, new JArray
             {
                 new JObject
                 {
-                    {
-                        Send.To, new JArray
-                        {
-                            new JObject
-                            {
-                                { "Email", email },
-                                { "Name", user.DisplayName }
-                            }
-                        }
-                    },
-                    { "TemplateID", 8136026 },
-                    {
-                        "Variables", new JObject
-                        {
-                            { "sender_sig", "Patrick & Éadaoin" },
-                            { "reset_link", resetLink },
-                            { "display_name", user.DisplayName }
-                        }
-                    }
+                    { "Email", email },
+                    { "Name", user.DisplayName }
                 }
+            })
+            .Property("Mj-TemplateID", 8136026)
+            .Property("Mj-TemplateLanguage", true)
+            .Property("Variables", new JObject
+            {
+                { "sender_sig", "Patrick & Éadaoin" },
+                { "reset_link", resetLink },
+                { "display_name", user.DisplayName }
             });
 
         await SendMailjetEmailAsync(request);
@@ -85,30 +75,23 @@ public class MailjetEmailSender(IMailjetClient mailjetClient) : MailjetService(m
             {
                 Resource = Send.Resource
             }
-            .Property(Send.Messages, new JArray
+            .Property(Send.FromEmail, _mailjetSettings.SenderEmail)
+            .Property(Send.FromName, _mailjetSettings.SenderName)
+            .Property(Send.Recipients, new JArray
             {
                 new JObject
                 {
-                    {
-                        Send.To, new JArray
-                        {
-                            new JObject
-                            {
-                                { "Email", email },
-                                { "Name", user.DisplayName }
-                            }
-                        }
-                    },
-                    { "TemplateID", 8136101 },
-                    {
-                        "Variables", new JObject
-                        {
-                            { "sender_sig", "Patrick & Éadaoin" },
-                            { "reset_code", resetCode },
-                            { "display_name", user.DisplayName }
-                        }
-                    }
+                    { "Email", email },
+                    { "Name", user.DisplayName }
                 }
+            })
+            .Property("Mj-TemplateID", 8136101)
+            .Property("Mj-TemplateLanguage", true)
+            .Property("Variables", new JObject
+            {
+                { "sender_sig", "Patrick & Éadaoin" },
+                { "reset_code", resetCode },
+                { "display_name", user.DisplayName }
             });
 
         await SendMailjetEmailAsync(request);
