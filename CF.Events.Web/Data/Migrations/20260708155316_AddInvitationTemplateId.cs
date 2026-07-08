4e6f0609b95608af161870beb61f71437126d782
Patrick Jominet <patrick.jominet@technologypartner.io>
2026-07-08 17:54:25 +0200

    added missing migration

---
+++ CF.Events.Web/Data/Migrations/20260708155316_AddInvitationTemplateId.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CF.Events.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInvitationTemplateId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InvitationTemplateId",
+                schema: "app",
+                table: "Events",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InvitationTemplateId",
+                schema: "app",
+                table: "Events");
+        }
+    }
+}
