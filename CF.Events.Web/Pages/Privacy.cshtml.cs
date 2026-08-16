8601e55ea78d380782733aebab16372cf49d2ee2
Patrick Jominet <patrick.jominet@technologypartner.io>
2026-08-16 17:38:00 +0200

    added privacy statement

---
+++ CF.Events.Web/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CF.Events.Web.Pages
+{
+    [AllowAnonymous]
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
