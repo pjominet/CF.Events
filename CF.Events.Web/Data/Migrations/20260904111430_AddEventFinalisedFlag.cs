303f38aeb0cb208eb639977153f4c287daeb78f5
Patrick Jominet <patrick.jominet@technologypartner.io>
2026-09-04 13:54:47 +0200

    added a not finalised warning to events in progress

---
+++ CF.Events.Web/Data/Migrations/20260904111430_AddEventFinalisedFlag.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CF.Events.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEventFinalisedFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFinalised",
+                schema: "app",
+                table: "Events",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFinalised",
+                schema: "app",
+                table: "Events");
+        }
+    }
+}
--- CF.Events.Web/Pages/Admin/EditEvent.cshtml.cs
+++ CF.Events.Web/Pages/Admin/EditEvent.cshtml.cs
@@ -58,6 +58,7 @@ public class EditEventModel(
                 DonationIban = @event.DonationIban,
                 DonationLink = @event.DonationLink,
                 BookingLinks = [.. @event.BookingLinks.Select(bl => bl.Link)],
+                IsFinalised = @event.IsFinalised,
                 FaqItems =
                 [
                     .. @event.EventFaq.OrderBy(f => f.SortOrder)
@@ -140,6 +141,7 @@ public class EditEventModel(
         @event.InvitationTemplateId = Event.InvitationEmailTemplateId;
         @event.EmailWithLink = Event.SendWithLink;
         @event.MaxParticipantsPerRsvp = Event.MaxParticipantsPerRsvp;
+        @event.IsFinalised = Event.IsFinalised;
 
         @event.DonationIban = Event.DonationTypes.Contains(DonationType.Iban) ? Event.DonationIban : null;
         @event.DonationLink = Event.DonationTypes.Contains(DonationType.Link) ? Event.DonationLink : null;
@@ -220,6 +222,7 @@ public class EditEventModel(
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string? Location { get; set; }
+        public bool IsFinalised { get; set; } = false;
         [Required] public string Description { get; set; } = null!;
         public string? TravelInstructions { get; set; }
 
